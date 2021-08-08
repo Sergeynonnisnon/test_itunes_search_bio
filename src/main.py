972d78b08d6c51bb748d8016314bbd645f7c67b5
nonnisnon <nonnisnon@gmail.com>
2021-08-08 16:10:01 +0300

    docker files add

--- src/main.py
+++ src/main.py
@@ -6,7 +6,9 @@ Part 1
 
 link: https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
 
-Using the itunes open API (link above), you need to implement a script that will return all songs (and information about them) that are included in this album by the parameters "artist" + "song title".
+Using the itunes open API (link above), you need to implement
+a script that will return all songs (and information about them)
+ that are included in this album by the parameters "artist" + "song title".
 
 Input format: any (config file, inside a script, input(), cli)
 Output format: .csv file (see attachment)
@@ -14,7 +16,9 @@ Output format: .csv file (see attachment)
 
 Part 2
 
-Using the parameters from the previous part ("artist" + "song title"), find the page with the chords of the song (take the first result from the search results), parse the text + chords and save them to a .txt file
+Using the parameters from the previous part ("artist" + "song title"),
+find the page with the chords of the song (take the first results from the search results),
+ parse the text + chords and save them to a .txt file
 
 If there are no results, leave the final file empty.
 
@@ -48,10 +52,10 @@ def first_part(input_song, input_artist):
         if input_song.lower() in str(track['trackName']).lower() \
                 and input_artist.lower() in str(track['artistName']).lower():
 
-            albums[track['artistName'] + '+' + str(track['collectionName']).replace(' ','+')] = track['collectionId']
+            albums[track['artistName'] + '+' + str(track['collectionName']).replace(' ', '+')] = track['collectionId']
 
             print(f'count albums with song is {len(albums)}')
-            artist_id=track['artistId']
+            artist_id = track['artistId']
 
     #######
     # get song from albums
@@ -64,18 +68,17 @@ def first_part(input_song, input_artist):
         response.encoding = response.apparent_encoding
         json_resp = response.json()
         json_resp = json_resp['results']
-        #check if album and artist correct
+        # check if album and artist correct
         for i in json_resp:
             if i['artistId'] != artist_id or i['collectionId'] not in albums.values():
                 json_resp.remove(i)
 
         result += json_resp
 
-
     print(f'count {len(result)} tracks')
 
     df = pd.DataFrame(result)
-    df.to_csv(f'{input_artist}_track_{input_song}.csv')
+    df.to_csv(f'results/{input_artist}_track_{input_song}.csv')
 
 
 #####################################################
@@ -87,18 +90,16 @@ def second_part(input_song, input_artist):
                             params={'q': f'{input_song} - {input_artist} lyrics  chords', 'format': 'json',
                                     'safesearch': 1})
 
-    print(f" first entry link{response.json()['results'][0]['pretty_url']}")
+    print(f" first entry link {response.json()['results'][0]['pretty_url']}")
     try:
         lyrics_chords = requests.get(response.json()['results'][0]['pretty_url'])
         soup = BS(lyrics_chords.content, 'lxml', ).body.get_text()
 
-        with open(f'text_{input_artist}_{input_song}.txt', 'w', encoding='utf-8') as f:
+        with open(f'results/text_{input_artist}_{input_song}.txt', 'w', encoding='utf-8') as f:
             f.write(soup)
 
-
     except IndexError:
-        with open(f'text_{input_artist}_{input_song}.txt', 'w') as f:
-            pass
+        open(f'results/text_{input_artist}_{input_song}.txt', 'w')
 
 
 if __name__ == '__main__':
