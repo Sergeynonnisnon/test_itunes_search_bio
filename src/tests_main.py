9bf834e3845486ab677141965356429d3f1bdbde
nonnisnon <nonnisnon@gmail.com>
2021-08-08 21:49:57 +0300

    bug fix

--- src/tests_main.py
+++ src/tests_main.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup as BS
 class Testmain(unittest.TestCase):
     def test_input_part_one(self):
         self.assertIsNone(first_part('!@##$%^&','*()_+~'))
-        self.assertRaises(ValueError,first_part,input_song, '')
-        self.assertRaises(ValueError,first_part,'', 'input_song')
+        self.assertRaises(ValueError,first_part,input_artist, '')
+        self.assertRaises(ValueError,first_part,'', input_song)
         self.assertRaises(TypeError,first_part,123,('',2))
 
