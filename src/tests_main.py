320f31402dd1fd1fc9fc1128f3cb39b3f0d94566
nonnisnon <nonnisnon@gmail.com>
2021-08-09 17:14:52 +0300

    pep8

--- src/tests_main.py
+++ src/tests_main.py
@@ -1,15 +1,11 @@
 import unittest
-from main import first_part,second_part
+from main import first_part, second_part
 from settings import input_song, input_artist
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup as BS
 
 
 class Testmain(unittest.TestCase):
     def test_input_part_one(self):
-        self.assertIsNone(first_part('!@##$%^&','*()_+~'))
-        self.assertRaises(ValueError,first_part,input_artist, '')
-        self.assertRaises(ValueError,first_part,'', input_song)
-        self.assertRaises(TypeError,first_part,123,('',2))
-
+        self.assertIsNone(first_part('!@##$%^&', '*()_+~'))
+        self.assertRaises(ValueError, first_part, input_artist, '')
+        self.assertRaises(ValueError, first_part, '', input_song)
+        self.assertRaises(TypeError, first_part, 123, ('', 2))
